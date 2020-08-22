52efc204bdaeecca0cd7a71089d06491efa7a3ae
Elliott <elliottjoiner@me.com>
2020-08-22 16:52:11 +0100

    updated index.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearningNode.js
 I will be using this repository to learn Node.js.
+
+Dependencies:
+Express: Transforms our package into a fully fledged web server.
+Nodemon: Allows our server to constantly update when we change a file.
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(`hello world`);
+//require
+const express = require('express');
+
+//initialise
+const app = express();
+
+//route handler
+//what to do when recieve HTTP request
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+//start listening for requests
+app.listen(3000, () => {
+    console.log('Listening on 3000');
+});
