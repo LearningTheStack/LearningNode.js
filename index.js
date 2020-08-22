358a3467ad5e7e83e4299d5b8f7de65b2ff20778
Elliott <elliottjoiner@me.com>
2020-08-22 18:24:35 +0100

    added a form and a body parser

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 //require
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //initialise
 const app = express();
 
+//app.use is applied to every incoming request
+//every request will be body parsed
+//parses body buffer to a useable object
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //route handler
 //what to do when recieve HTTP request
+//root address (GET request)
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.send(`
+     <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password Confirmation" />
+        <button>Sign Up</button>
+      </form>
+     </div>
+    `);
+});
+
+
+//root address (POST request from above form)
+app.post('/', (req,res) => {
+    console.log(req.body);
+    res.send('Account created!');
 });
 
 //start listening for requests
