49686c3a6051e68e2053301918cc848044027563
LearningTheStack <elliott.joiner@googlemail.com>
2020-09-01 16:19:27 +0100

    completed cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -34,9 +36,36 @@ router.post('/cart/products', async (req, res) => {
     });
            
 
-    res.send('Product added to cart');
+    res.redirect('/cart');
 });
+
 // receieve get request to show all items in cart
+router.get('/cart', async (req, res) => {
+    if(!req.session.cartId) {
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        //item === { id: , quanitity: }
+        const product = await productsRepo.getOne(item.id);
+        
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+   const { itemId } = req.body;
+   const cart = await cartsRepo.getOne(req.session.cartId);
+
+   const items = cart.items.filter(item => item.id !== itemId);
+
+   await cartsRepo.update(req.session.cartId, { items });
+   res.redirect('/cart');
+});
 
 //receive post request to delete
 
